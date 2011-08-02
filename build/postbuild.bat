4f183eff041d85d7b5d7da402a4985660276583e
aarondrabeck <github@aarondrabeck.com>
2011-08-02 23:54:25 +0000

    Moving 1.5 to branches and 2.1 to trunk

--- build/postbuild.bat
+++
@@ -1,7 +0,0 @@
-ECHO parameter=%1
-CD %1
-COPY "zVirtualScenes.exe" "..\..\latest_build\zVirtualScenes.exe"
-COPY "HTTP INTERFACE READ ME.txt" "..\..\latest_build\HTTP INTERFACE READ ME.txt"
-COPY "RAW SOCKET READ ME.txt" "..\..\latest_build\RAW SOCKET READ ME.txt"
-
-
