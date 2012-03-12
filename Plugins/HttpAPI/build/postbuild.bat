4f91cea2628763230bbe4875c8bcd44357d6af7d
aarondrabeck <github@aarondrabeck.com>
2012-03-12 15:56:44 +0000

    - Sencha App Restructuring in 2 commits

--- Plugins/HttpAPI/build/postbuild.bat
+++ Plugins/HttpAPI/build/postbuild.bat
@@ -2,9 +2,16 @@ ECHO parameter=%1
 CD %1
 IF EXIST "htdocs" (RMDIR htdocs /s /q)
 md "htdocs"
+CD htdocs
+IF EXIST "sencha" (RMDIR htdocs /s /q)
+md "sencha"
 CD %1
 xcopy /Y /s "..\..\Plugins\HttpAPI\htdocs\*" "htdocs\*"
-
+xcopy /Y "..\..\Plugins\HttpAPI\sencha\index.html" "htdocs\sencha\"
+xcopy /Y "..\..\Plugins\HttpAPI\sencha\all-classes.js" "htdocs\sencha\"
+xcopy /Y "..\..\Plugins\HttpAPI\sencha\app.js" "htdocs\sencha\"
+xcopy /Y /s "..\..\Plugins\HttpAPI\sencha\touch\sencha-touch.js" "htdocs\sencha\touch\"
+xcopy /Y /s "..\..\Plugins\HttpAPI\sencha\resources\*" "htdocs\sencha\resources\*"
 
 
 
--- Plugins/HttpAPI/htdocs/sencha/app/model/Device.js
+++
@@ -1,7 +0,0 @@
-﻿Ext.define('zvsMobile.model.Device', {
-    extend: 'Ext.data.Model',
-
-    config: {
-        fields: ['id', 'name', 'on_off', 'level', 'level_txt', 'type']
-    }
-});
