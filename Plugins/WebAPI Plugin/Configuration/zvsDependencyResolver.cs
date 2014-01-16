a1223cebf3319d59c06841f01a9c1a960b748e0a
Rob Chartier <rob.chartier@gmail.com>
2014-01-16 19:07:41 +0000

    added a DeviceCommandController, which exposes a REST Resource DeviceCommand, which has a GET to list the device commands, and a POST to execute the device command.  You will need to pass in the X-zvsToken header in both cases.  In the case of POST, send a application/json formatted object with DeviceID (int), CommandId (int), and Value (string)

--- Plugins/WebAPI Plugin/Configuration/zvsDependencyResolver.cs
+++ Plugins/WebAPI Plugin/Configuration/zvsDependencyResolver.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Dependencies;
 using WebAPI.Controllers;
+using zvs.Processor.Logging;
 
 namespace WebAPI.Configuration
 {
     public class zvsDependencyResolver : IDependencyResolver
     {
+        private ILog _log = zvs.Processor.Logging.LogManager.GetLogger<zvsDependencyResolver>();
         public IDependencyScope BeginScope()
         {
             // This example does not support child scopes, so we simply return 'this'.
@@ -20,9 +22,11 @@ namespace WebAPI.Configuration
         public object GetService(Type serviceType)
         {
             //We will give each controller zvsAuthenticatedControllerBase based controller reference to the WebAPIPlugin 
-            if (typeof(zvsController).IsAssignableFrom(serviceType))
+            if (typeof (zvsController).IsAssignableFrom(serviceType))
             {
-                return Activator.CreateInstance(serviceType, WebAPIPlugin);
+                var c = Activator.CreateInstance(serviceType, WebAPIPlugin);
+                _log.InfoFormat("Resolved: {0} to: {1}", serviceType.FullName, c.GetType().FullName);
+                return c;
             }
             return null;
         }
