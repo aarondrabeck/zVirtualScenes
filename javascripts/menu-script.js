c331d06de6d51ece5ceaa12df27bcc4cb24640df
Aaron Drabeck <aaron-drabeck@leavitt.com>
2014-12-30 22:24:30 -0700

    - Moved documentation over from Google Code -

--- javascripts/menu-script.js
+++ javascripts/menu-script.js
@@ -1,110 +1,111 @@
-(function($) {
+(function ($) {
 
-  $.fn.menumaker = function(options) {
-      
-      var cssmenu = $(this), settings = $.extend({
-        title: "Menu",
-        format: "dropdown",
-        sticky: false
-      }, options);
+    $.fn.menumaker = function (options) {
 
-      return this.each(function() {
-        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-        $(this).find("#menu-button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
-            mainmenu.hide().removeClass('open');
-          }
-          else {
-            mainmenu.show().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
+        var cssmenu = $(this), settings = $.extend({
+            title: "Menu",
+            format: "dropdown",
+            sticky: false
+        }, options);
+
+        return this.each(function () {
+            cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+            $(this).find("#menu-button").on('click', function () {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.hide().removeClass('open');
+                }
+                else {
+                    mainmenu.show().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
+                }
+            });
+
+            cssmenu.find('li ul').parent().addClass('has-sub');
+
+            multiTg = function () {
+                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                cssmenu.find('.submenu-button').on('click', function () {
+                    $(this).toggleClass('submenu-opened');
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').hide();
+                    }
+                    else {
+                        $(this).siblings('ul').addClass('open').show();
+                    }
+                });
+            };
+
+            if (settings.format === 'multitoggle') multiTg();
+            else cssmenu.addClass('dropdown');
+
+            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+            resizeFix = function () {
+                if ($(window).width() > 768) {
+                    cssmenu.find('ul').show();
+                }
+
+                if ($(window).width() <= 768) {
+                    cssmenu.find('ul').hide().removeClass('open');
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
+
         });
-
-        cssmenu.find('li ul').parent().addClass('has-sub');
-
-        multiTg = function() {
-          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-          cssmenu.find('.submenu-button').on('click', function() {
-            $(this).toggleClass('submenu-opened');
-            if ($(this).siblings('ul').hasClass('open')) {
-              $(this).siblings('ul').removeClass('open').hide();
-            }
-            else {
-              $(this).siblings('ul').addClass('open').show();
-            }
-          });
-        };
-
-        if (settings.format === 'multitoggle') multiTg();
-        else cssmenu.addClass('dropdown');
-
-        if (settings.sticky === true) cssmenu.css('position', 'fixed');
-
-        resizeFix = function() {
-          if ($( window ).width() > 768) {
-            cssmenu.find('ul').show();
-          }
-
-          if ($(window).width() <= 768) {
-            cssmenu.find('ul').hide().removeClass('open');
-          }
-        };
-        resizeFix();
-        return $(window).on('resize', resizeFix);
-
-      });
-  };
+    };
 })(jQuery);
 
-(function($){
-$(document).ready(function(){
+(function ($) {
+    $(document).ready(function () {
 
-$(document).ready(function() {
-  $("#cssmenu").menumaker({
-    title: "Menu",
-    format: "multitoggle"
-  });
+        $(document).ready(function () {
+            $("#cssmenu").menumaker({
+                title: "Menu",
+                format: "multitoggle"
+            });
 
-  $("#cssmenu").prepend("<div id='menu-line'></div>");
+            $("#cssmenu").prepend("<div id='menu-line'></div>");
 
-var foundActive = false, activeElement, linePosition = 0, menuLine = $("#cssmenu #menu-line"), lineWidth, defaultPosition, defaultWidth;
+            var foundActive = false, activeElement, linePosition = 0, menuLine = $("#cssmenu #menu-line"), lineWidth, defaultPosition, defaultWidth;
 
-$("#cssmenu > ul > li").each(function() {
-  if ($(this).hasClass('active')) {
-    activeElement = $(this);
-    foundActive = true;
-  }
-});
+            $("#cssmenu > ul > li").each(function () {
+                if ($(this).hasClass('active')) {
+                    activeElement = $(this);
+                    foundActive = true;
+                }
+            });
 
-if (foundActive === false) {
-  activeElement = $("#cssmenu > ul > li").first();
-}
+            if (foundActive === false) {
+                activeElement = $("#cssmenu > ul > li").first();
+            }
 
-defaultWidth = lineWidth = activeElement.width();
+            defaultWidth = lineWidth = activeElement.width();
 
-defaultPosition = linePosition = activeElement.position().left;
+            if (activeElement.position()) {
+                defaultPosition = linePosition = activeElement.position().left;
+            }
+            menuLine.css("width", lineWidth);
+            menuLine.css("left", linePosition);
 
-menuLine.css("width", lineWidth);
-menuLine.css("left", linePosition);
+            $("#cssmenu > ul > li").hover(function () {
+                activeElement = $(this);
+                lineWidth = activeElement.width();
+                linePosition = activeElement.position().left;
+                menuLine.css("width", lineWidth);
+                menuLine.css("left", linePosition);
+            },
+            function () {
+                menuLine.css("left", defaultPosition);
+                menuLine.css("width", defaultWidth);
+            });
 
-$("#cssmenu > ul > li").hover(function() {
-  activeElement = $(this);
-  lineWidth = activeElement.width();
-  linePosition = activeElement.position().left;
-  menuLine.css("width", lineWidth);
-  menuLine.css("left", linePosition);
-}, 
-function() {
-  menuLine.css("left", defaultPosition);
-  menuLine.css("width", defaultWidth);
-});
-
-});
+        });
 
 
-});
+    });
 })(jQuery);
